1fd6575d562400570cf58c166f3702368554ff79
arvavit <arvavitcorp@gmail.com>
2024-06-04 01:34:19 +0300

    fd

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,10 +3,11 @@ const config = require('config');
 
 module.exports = function(req, res, next) {
   // Get token from header
-  const token = req.header('x-auth-token');
+  const token = req.header('Authorization');
 
   // Check if no token
   if (!token) {
+    console.log('No token provided');
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
@@ -17,10 +18,12 @@ module.exports = function(req, res, next) {
       return res.status(500).json({ msg: 'JWT secret is not defined in configuration' });
     }
 
-    const decoded = jwt.verify(token, jwtSecret);
-    req.user = decoded.user;
+    const decoded = jwt.verify(token.split(' ')[1], jwtSecret);
+    req.user = { id: decoded.id }; // Ensure req.user has the expected structure
+    console.log('Token is valid:', decoded);
     next();
   } catch (err) {
+    console.log('Token is not valid:', err.message);
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
