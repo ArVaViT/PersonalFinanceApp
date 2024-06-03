d095b56410608e96e1ea42f6a71d5ec6f4d68374
arvavit <arvavitcorp@gmail.com>
2024-06-03 06:28:53 +0300

    Update client to use Heroku API

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./db');
 const cors = require('cors');
 const config = require('config');
@@ -6,34 +7,30 @@ const config = require('config');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Проверка наличия переменной MONGODB_URI
-const db = config.get('MONGODB_URI');
-if (!db) {
-    console.error('FATAL ERROR: MONGODB_URI is not defined.');
-    process.exit(1);
-}
-
-// Подключение к базе данных
-connectDB().then(() => {
-  console.log('MongoDB Connected...');
-}).catch(err => {
-  console.error('Failed to connect to MongoDB:', err);
-  process.exit(1);
-});
-
 // Middleware для обработки JSON
 app.use(express.json({ extended: false }));
 
 // Настройки CORS для безопасности
 const corsOptions = {
-  origin: 'https://my-personal-finance-app-1e2eb0485e32.herokuapp.com', // Укажите точный адрес вашего фронтенда для продакшена
+    origin: 'https://your-vercel-app-name.vercel.app', // Укажите точный адрес вашего фронтенда для продакшена
     optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
+// Обслуживание статических файлов
+app.use(express.static(path.join(__dirname, 'public')));  // <-- добавьте эту строку
+
 // Корневой маршрут
 app.get('/', (req, res) => {
     res.send('API is running');
+});
+
+// Подключение к базе данных
+connectDB().then(() => {
+    console.log('MongoDB Connected...');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);
 });
 
 // Маршруты API
